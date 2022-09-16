c971df6987411822048c7887d57786c409afcb54
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-09-16 19:09:07 +0300

    Response to the request

---
+++ FlowerShop.Application.Contracts/Outgoing/Response.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop.Application.Contracts.Outgoing
+{
+    public class Response
+    {
+        public bool IsSuccess { get; init; }
+
+        public static Response Success => new() { IsSuccess = true };
+        public static Response Error => new() { IsSuccess = false };
+    }
+}
