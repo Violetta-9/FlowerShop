8ef62ab586697b02a56e7422a3696717af1a3c83
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-09-16 19:06:13 +0300

    Command and HandlerCommand for registration

---
+++ FlowerShop.Application/Command/User/RegisterCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlowerShop.Application.Contracts.Incoming;
+using FlowerShop.Application.Contracts.Outgoing;
+using MediatR;
+
+namespace FlowerShop.Application.Command.User
+{
+    public class RegisterCommand:IRequest<Response>
+    {
+        public AddUserDTO NewUser { get; set; }
+
+        public RegisterCommand(AddUserDTO newUser)
+        {
+            NewUser = newUser;
+        }
+    }
+}
---
+++ FlowerShop.Application/CommandHandler/User/RegisterCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlowerShop.Application.Command.User;
+using FlowerShop.Application.Configurations.User;
+using FlowerShop.Application.Contracts.Outgoing;
+using FlowerShop.Data.Domain.Models;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace FlowerShop.Application.CommandHandler.User
+{
+    public class RegisterCommandHandler : IRequestHandler<RegisterCommand, Response>
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ShopUser> _userManager;
+        public RegisterCommandHandler(RoleManager<IdentityRole> roleManager, UserManager<ShopUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task<Response> Handle(RegisterCommand request, CancellationToken cancellationToken)
+        {
+            var roleName = UserRole.User;
+            var user = new ShopUser(request.NewUser.Login, request.NewUser.Email, request.NewUser.LastName,
+                request.NewUser.FirstName, request.NewUser.PhoneNumber);
+            var createUser =await _userManager.CreateAsync(user,request.NewUser.Password);
+            if (!createUser.Succeeded)
+            {
+                //todo:
+            }
+
+            if (!await _roleManager.RoleExistsAsync(roleName))
+            {
+                var role = new IdentityRole(roleName);
+                await _roleManager.CreateAsync(role);
+            }
+            await _userManager.AddToRoleAsync(user,roleName);
+            await _userManager.UpdateAsync(user);
+            return Response.Success;
+        }
+    }
+}
