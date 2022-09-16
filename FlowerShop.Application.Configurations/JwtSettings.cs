f77a80aa85ba81f54c6f034c01d0719432cbc174
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-09-16 19:07:34 +0300

    Settings

---
+++ FlowerShop.Application.Configurations/JwtSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop.Application.Configurations
+{
+    public class JwtSettings
+    {
+        public string JwtSecretKey { get; set; }
+
+        public JwtSettings() { }
+
+        public JwtSettings(string jwtSecretKey)
+        {
+            JwtSecretKey = jwtSecretKey;
+        }
+    }
+}
