3f04d9f89f2207a081ec514091394b31acf73ce3
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-09-16 19:03:11 +0300

    Add Mapping for User

--- FlowerShop.Data.Share/MappingConfigurations/UserMappingConfiguration.cs
+++ FlowerShop.Data.Share/MappingConfigurations/UserMappingConfiguration.cs
@@ -6,13 +6,13 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace FlowerShop.Data.Share.MappingConfigurations
 {
-    public class UserMappingConfiguration : IEntityTypeConfiguration<User>
+    public class UserMappingConfiguration : IEntityTypeConfiguration<ShopUser>
     {
-        public void Configure(EntityTypeBuilder<User> builder)
+        public void Configure(EntityTypeBuilder<ShopUser> builder)
         {
-            builder.Property(x => x.FirstName).HasColumnName(nameof(User.FirstName)).HasMaxLength(256)
+            builder.Property(x => x.FirstName).HasColumnName(nameof(ShopUser.FirstName)).HasMaxLength(256)
                 .IsRequired();
-            builder.Property(x => x.LastName).HasColumnName(nameof(User.LastName)).HasMaxLength(256)
+            builder.Property(x => x.LastName).HasColumnName(nameof(ShopUser.LastName)).HasMaxLength(256)
                 .IsRequired();
         }
     }
