867675da87a2ba4f9fa9ba0ce3c97f5f8f434bb8
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-09-16 19:05:12 +0300

    Add Controller with registration, login

--- FlowerShop/Controllers/UserController.cs
+++ FlowerShop/Controllers/UserController.cs
@@ -1,9 +1,13 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using FlowerShop.Application.Command.User;
 using FlowerShop.Application.Contracts.Incoming;
+using FlowerShop.Application.Queries;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace FlowerShop.Controllers
 {
@@ -11,6 +15,54 @@ namespace FlowerShop.Controllers
     [ApiController]
     public class UserController:ControllerBase
     {
-        
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator=mediator;
+        }
+        [HttpPost("registration")]
+        [SwaggerResponse(StatusCodes.Status200OK, Type = typeof(string))]
+        [SwaggerOperation(Summary = "Register new user", OperationId = "RegisterUser")]
+        public async Task<ActionResult> RegisterUser([FromBody]AddUserDTO newUser)
+        {
+            var query = new RegisterCommand(newUser);
+            if (query == null)
+            {
+                throw new ArgumentNullException(nameof(query));
+            }
+
+            var result = await _mediator.Send(query);
+
+            if (result == null)
+            {
+                return NotFound(result);
+            }
+
+            return Ok(result);
+        }
+        [HttpPost("login")]
+        [SwaggerResponse(StatusCodes.Status200OK, Type = typeof(string))]
+        [SwaggerOperation(Summary = "Login", OperationId = "Login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO loginData)
+        {
+            var query = new LoginUserQueries(loginData);
+
+            if(query == null)
+            {
+                throw new ArgumentNullException(nameof(query));
+            }
+
+            var result = await _mediator.Send(query);
+
+            if (result == null)
+            {
+                return NotFound(result);
+            }
+
+            return Ok(result);
+            
+        }
     }
+
 }
