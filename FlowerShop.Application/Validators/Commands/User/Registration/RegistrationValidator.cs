7e254753e5e6d6619f50e1f0f3223cbee41ab436
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-10-17 18:00:47 +0300

    validator for registration

---
+++ FlowerShop.Application/Validators/Commands/User/Registration/RegistrationValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlowerShop.Application.Command.User;
+using FluentValidation;
+
+namespace FlowerShop.Application.Validators.Commands.User.Registration
+{
+    public class RegistrationValidator:AbstractValidator<RegisterCommand>
+    {
+        public RegistrationValidator()
+        {
+            CreateRules();
+        }
+
+        private void CreateRules()
+        {
+            RuleFor(x => x.NewUser.Email)
+                .Cascade(CascadeMode.Stop);
+            //todo:
+
+        }
+
+       
+    }
+}
