e140d54bbacdf350ab67794bc9e6c55b001f3ff6
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-10-20 18:11:44 +0300

    add postprocessor

---
+++ FlowerShop.Application/PostProcessor/Product/AddProductPostProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlowerShop.Application.Command.Product;
+using MediatR.Pipeline;
+
+namespace FlowerShop.Application.PostProcessor.Product
+{
+    public class AddProductPostProcessor : IRequestPostProcessor<AddProductCommand, long>
+    {
+        public async Task Process(AddProductCommand request, long response, CancellationToken cancellationToken)
+        {
+           Console.WriteLine("sssssssssssssssssssssssssssssssssssssssssssssssssssssssss");
+           await Task.Delay(2);
+        }
+    }
+}
