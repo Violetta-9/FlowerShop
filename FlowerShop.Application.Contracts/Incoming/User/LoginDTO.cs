46dd58c420a37c94496a7df67cafced3567b0442
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-10-20 18:10:30 +0300

    update

---
+++ FlowerShop.Application.Contracts/Incoming/User/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop.Application.Contracts.Incoming.User
+{
+    public class LoginDTO
+    {
+        public string UserLoginOrEmail { get; set; }
+        public string Password { get; set; }
+    }
+}
