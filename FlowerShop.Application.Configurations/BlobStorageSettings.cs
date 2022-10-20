79703ab67d7c355654925ae09b6b2628bcee0441
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-10-20 18:11:04 +0300

    Blob settings

---
+++ FlowerShop.Application.Configurations/BlobStorageSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop.Application.Configurations
+{
+    public  class BlobStorageSettings
+    {
+       public string PathTemplate { get; set; }
+       public string ProductImagesContainer { get; set; }
+    }
+}
