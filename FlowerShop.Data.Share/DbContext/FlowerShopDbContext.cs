a894abaf8b9f8dcf171b2fe2b79e5ff9d7ead6ca
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-10-20 18:10:11 +0300

    update

--- FlowerShop.Data.Share/DbContext/FlowerShopDbContext.cs
+++ FlowerShop.Data.Share/DbContext/FlowerShopDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using FlowerShop.Data.Domain.Models;
 using FlowerShop.Data.Share.MappingConfigurations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -9,8 +10,13 @@ namespace FlowerShop.Data.Share.DbContext
 {
     public class FlowerShopDbContext : IdentityDbContext<ShopUser>
     {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+
         public FlowerShopDbContext(DbContextOptions options) : base(options)
         {
+            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
