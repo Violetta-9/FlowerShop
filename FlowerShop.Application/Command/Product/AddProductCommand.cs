6b25d066e7aa0974da96c604d7b35c2d8cf3f906
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-10-24 19:27:00 +0300

    Command for product

--- FlowerShop.Application/Command/Product/AddProductCommand.cs
+++ FlowerShop.Application/Command/Product/AddProductCommand.cs
@@ -5,14 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using FlowerShop.Application.Contracts.Incoming.Product;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 
 namespace FlowerShop.Application.Command.Product
 {
     public class AddProductCommand:IRequest<long>
     {
-        public AddProductCommand(AddProductDTO product)
+        public AddProductDTO Product { get; set; }
+        public IFormFile[] Files;
+        public AddProductCommand(AddProductDTO product,IFormFile[] files)
         {
-
+            Product=product;
+            Files=files;
         }
     }
 }
