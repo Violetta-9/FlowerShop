c5f169299544f6762e7b11a15e6089edd4c25403
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-10-29 13:41:11 +0300

    Dto

---
+++ FlowerShop.Application.Contracts/Outgoing/ProductImageDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop.Application.Contracts.Outgoing
+{
+    public class ProductImageDTO
+    {
+        public string Path { get; set; }
+    }
+}
