ce7254099c15d746d3b2010fa7077c86ac781876
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-10-29 13:42:40 +0300

    Query

---
+++ FlowerShop.Application/Queries/Product/GetProductByCategoryIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlowerShop.Application.Contracts.Outgoing;
+using MediatR;
+
+namespace FlowerShop.Application.Queries.Product
+{
+    public class GetProductByCategoryIdQuery : IRequest<ProductDTO[]>
+    {
+        public long CategoryId { get; set; }
+        public GetProductByCategoryIdQuery(long categoryId)
+        {
+            CategoryId = categoryId;
+        }
+    }
+}
---
+++ FlowerShop.Application/Queries/ProductImage/GetProductImageByProductIdQueries.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlowerShop.Application.Contracts.Outgoing;
+using MediatR;
+
+namespace FlowerShop.Application.Queries.ProductImage
+{
+    public class GetProductImageByProductIdQueries : IRequest<ProductImageDTO[]>
+    {
+        public long ProductId { get; set; }
+        public GetProductImageByProductIdQueries(long productId)
+        {
+            ProductId = productId;
+        }
+    }
+}
