df7e2f3554328aac3baf255575331d2d9d08007c
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-11-16 19:17:31 +0300

    Add new DTO

---
+++ FlowerShop.Application.Contracts/Incoming/ProductForCard/ProductForCardDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop.Application.Contracts.Incoming.ProductForCard
+{
+    public class ProductForCardDTO
+    {
+        public long ProductId { get; set; }
+        public int Quentity { get; set; }
+        public string UserName { get; set; }
+    }
+}
---
+++ FlowerShop.Application.Contracts/Outgoing/OrdersDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop.Application.Contracts.Outgoing
+{
+    public  class OrdersDTO
+    {
+        public string UserAdress { get; set; }
+        public string[] FlowerTitle { get; set; }
+        public string Phone { get; set; }
+        public double TotalPrice { get; set; }
+        public DateTime TimeOfOrder { get; set; }
+        public bool IsCompleted { get; set; } = false;
+    }
+}
